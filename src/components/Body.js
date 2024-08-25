04d40e86ecb20dec3fe6334c057162f04cd89e46
pranavikadatla <datlapranavika205@gmail.com>
2024-08-25 21:23:21 +0530

    changed the api

--- src/components/Body.js
+++ src/components/Body.js
@@ -13,11 +13,11 @@ const Body=()=>{
   async function fetchData(){
     const data =await fetch ("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.537122&lng=73.6771662&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
     const json=await data.json();
-    setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-    setFilterRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants) 
+    setListOfRestaurants(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    setFilterRestaurants(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants) 
   }
   const {loggedInUser,setUserName}=useContext(UserContext); 
-    return listOfRestaurants.length===0 ? <Shimmer/> : (
+    return listOfRestaurants?.length===0 ? <Shimmer/> : (
       <div className="mx-5 ">
         <div className="flex my-6 ">
              <div className="border-2 rounded-lg  w-[50%] flex">
