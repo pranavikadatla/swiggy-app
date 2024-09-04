a8258e7b222de97f6fe347ae5eed0a0b4e2ac764
pranavikadatla <datlapranavika205@gmail.com>
2024-09-04 23:38:15 +0530

    menu cors resolved

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
 export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 export const LOGO_URL="https://cdn.create.vista.com/api/media/small/81706150/stock-vector-delivery-boy-ride-scooter-motorcycle-service-with-icons"
-export const MENU_API="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.537122&lng=73.6771662&restaurantId=";
+export const MENU_API="https://corsproxy-la3g.onrender.com/full/?url=https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5209639&lng=73.7786898&restaurantId=";
+
+//https://corsproxy-la3g.onrender.com/full/?url=https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5209639&lng=73.7786898&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING
